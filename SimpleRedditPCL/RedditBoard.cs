e5ba9a57dc4f44a155e96e335a3268c2ec37b18a
Nick Hodge <hodgenick@gmail.com>
2013-04-29 21:45:32 +1000

    Initial commit
    First and initial commit

---
+++ SimpleRedditPCL/RedditBoard.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace SimpleRedditPCL
+{
+    public class RedditBoard
+    {
+        public string Subreddit;
+ 
+        public RedditBoard(string sr)
+        {
+            Subreddit = sr;
+        }
+
+        public async Task<JEnumerable<JToken>>GetData()
+        {
+            var wc = new HttpClient();
+            var dt = await wc.GetStringAsync(String.Format("http://reddit.com/r/{0}.json",Subreddit));
+            var ls = JObject.Parse(dt);
+            return ls["data"]["children"].Children();
+        }
+    }
+
+    public class subredditentries
+    {
+        private static DateTime unixDateTimeEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0);
+        public string domain { get; set; }
+        public string subreddit { get; set; }
+        public string selftext_html { get; set; }
+        public string selftext { get; set; }
+        public string likes { get; set; }
+        public string link_flair_text { get; set; }
+        public string id { get; set; }
+        public bool clicked { get; set; }
+        public string title { get; set; }
+        public int score { get; set; }
+        public string approved_by { get; set; }
+        public bool over_18 { get; set; }
+        public bool hidden { get; set; }
+        public string thumbnail { get; set; }
+        public string subreddit_id { get; set; }
+        public bool edited { get; set; }
+        public int downs { get; set; }
+        public bool saved { get; set; }
+        public bool is_self { get; set; }
+        public string permalink { get; set; }
+        public string name { get; set; }
+        public string url { get; set; }
+        public string author { get; set; }
+        public long created_utc { get; set; }
+        public int ups { get; set; }
+        public int num_comments { get; set; }
+
+        public string TitleClean
+        {
+            get { return WebUtility.HtmlDecode(title); }
+        }
+
+        public DateTime CreatedUTCTime
+        {
+            get
+            {
+                return unixDateTimeEpoch.AddSeconds(created_utc);
+            }
+        }
+
+        public DateTime CreatedLocalTime
+        {
+            get
+            {
+                return unixDateTimeEpoch.AddSeconds(created_utc).ToLocalTime();
+            }
+        }
+
+
+    }
+
+}
---
+++ SimpleRedditPCL/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Newtonsoft.Json.Linq;
+using SimpleRedditPCL;
+
+namespace SimpleRedditPCL
+{
+    public class MainViewModel : BindableBase
+    {
+        public RedditBoard redditData;
+        
+        public MainViewModel()
+        {
+            this.Items = new ObservableCollection<subredditentries>();
+            this.Subreddit = "pics";
+            this.redditData = new RedditBoard(this.Subreddit);
+        }
+
+        public ObservableCollection<subredditentries> Items { get; private set; }
+
+        private bool _isDataLoaded = false;
+        public Boolean IsDataLoaded
+        {
+            get { return this._isDataLoaded; }
+            set { this.SetProperty(ref this._isDataLoaded, value); }
+        }
+
+        private string _subreddit = "";
+        public string Subreddit
+        {
+            get { return this._subreddit; }
+            set { this.SetProperty(ref this._subreddit, value); }
+        }
+
+        public async void LoadData()
+        {
+            // Sample data; replace with real data
+            //this.Items.Add(new subredditentries() { Title = "runtime one"});
+            var srh = await redditData.GetData();
+            if (srh.Any())
+            {
+                Items.Clear();
+                var i = 0;
+                foreach (var sre in srh)
+                {
+                    var x = sre["data"].ToObject<subredditentries>();
+                    Items.Add(x);
+                }
+            }
+
+            this.IsDataLoaded = true;
+        }
+
+    }
+}
